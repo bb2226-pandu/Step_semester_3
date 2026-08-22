1eef02e5b8288795aabb16b861ecfe4d4a4ecc9e
bb2226-pandu <bb2226@srmist.edu.in>
2026-08-22 16:37:17 +0530

    Add armstrong program

---
+++ Armstrong.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Armstrong{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int original = number;
+        int sum = 0 ;
+        while(number>0){
+            int digit = number%10;
+            sum = sum + digit*digit*digit;
+            number = number/10;
+        }
+        if(sum == original){
+            System.out.println("armstrong number");
+        }else{
+            System.out.println("Not an armstrong number");
+        }
+    }
+}
